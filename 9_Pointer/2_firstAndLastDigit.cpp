1751870480843f7886f8ca0c5b73eea58af3cace
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-05-11 21:52:11 +0530

    updated

--- 9_Pointer/2_firstAndLastDigit.cpp
+++ 9_Pointer/2_firstAndLastDigit.cpp
@@ -2,22 +2,26 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-void firstAndLastDigit(int *n)
+void firstAndLastDigit(int *n,int *firstDigit,int *lastDigit)
 {
     int temp = *n;
-    cout << "last digit of number is " << temp % 10 << endl;
+    *lastDigit=temp%10;
     while (temp > 9)
     {
         temp = temp / 10;
     }
-    cout << "first digit of number is " << temp << endl;
+    *firstDigit=temp;
+    return;
 }
 int main()
 {
     int a;
     cout<<"enter the number"<<endl;
     cin>>a;
-    firstAndLastDigit(&a);
+    int firstDigit,lastDigit;
+    firstAndLastDigit(&a,&firstDigit,&lastDigit);
+    cout<<"first digit is "<<firstDigit<<endl;
+    cout<<"last digit is "<<lastDigit<<endl;
 
 
     return 0;
