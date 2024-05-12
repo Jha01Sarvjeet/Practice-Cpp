ff2be988862a72c603ea5656519c4ab60895c3ba
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-05-12 08:23:06 +0530

    Null pointer declaration

---
+++ 9_Pointer/3_NUllPointer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{   //way to declare empty pointer variable
+    int *ptr1 = NULL;
+    int *ptr2 = 0;
+    int *ptr3 = '\0';
+    int *ptr; // not preffered to declare empty pointer
+    cout << ptr1 << " " << ptr2 << " " << ptr3 << " " << ptr << endl;
+
+    return 0;
+}
