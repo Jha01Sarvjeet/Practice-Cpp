42878fe078d7a8f8c47525a0c08f26dabc5af5a6
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-05-13 23:24:50 +0530

    updtaed with optimal solution

--- 10_array/8_duplicate_in_array.cpp
+++ 10_array/8_duplicate_in_array.cpp
@@ -2,26 +2,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 bool duplicate(int arr[], int n)
+
 {
+    map<int,int>mpp;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int count = 0;
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         if (arr[i] == arr[j])
+    //             count++;
+    //     }
+    //     if (count > 1)
+    //         {return true;
+    //         break;}
+        
+    // }
+    // return false;
     for (int i = 0; i < n; i++)
     {
-        int count = 0;
-        for (int j = 0; j < n; j++)
-        {
-            if (arr[i] == arr[j])
-                count++;
-        }
-        if (count > 1)
-            {return true;
-            break;}
-        
+        mpp[arr[i]]++;
     }
-    return false;
+    for(auto i:mpp){
+        // cout<<i.first<<"  "<<i.second<<endl;
+        if(i.second>1)
+            {return true;}
+            
+    }
+    // return false;
 }
 int main()
 {
 
-    int arr[]={2,3,6,5,7,8,1,9};
+    int arr[]={2,3,6,5,7,1,1,9};
     bool ans=duplicate(arr,8);
     if(ans)
         cout<<"duplicate found"<<endl;
