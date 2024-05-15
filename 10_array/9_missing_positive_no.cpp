e869ea44a2ef684837c001557e3e9e234249bd85
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-05-15 10:47:46 +0530

    update the solution using map function

--- 10_array/9_missing_positive_no.cpp
+++ 10_array/9_missing_positive_no.cpp
@@ -3,23 +3,37 @@
 using namespace std;
 /* to find the smallest missing positive element in the sorted Array that contains only positive
 elements.*/
+// int missing_number(int arr[],int n){
+//     int x=0;
+//     bool flag=false;
+//     for(int i=0;i<n;i++){
+//         if(arr[i]!=x){
+//             return x;
+//             flag=true;
+//             break;
+//         }
+//         else{
+//             x++;
+//         }
+//     }
+//     return -1;
+// }
+//Using hash map
 int missing_number(int arr[],int n){
-    int x=0;
-    bool flag=false;
+    map<int,int>mpp;
     for(int i=0;i<n;i++){
-        if(arr[i]!=x){
-            return x;
-            flag=true;
-            break;
-        }
-        else{
-            x++;
+        mpp[arr[i]]++;
+    }
+    for(int i=0;i<n;i++){
+        if(mpp[i]==0){
+            return i;
         }
     }
     return -1;
+
 }
 int main(){
-int arr[]={0,1,3,4,5,6};
+int arr[]={1,2,3,4,5,6};
 int ans=missing_number(arr,6);
 if(ans==-1){
     cout<<"no missing number"<<endl;
