f2f0f9b9cd3c1583db3ffda5fcc43b5f6b3920b9
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-05-21 12:46:48 +0530

    updated with DNf(Dutch National Flag ) Algo.

--- 10_array/16_sort012.cpp
+++ 10_array/16_sort012.cpp
@@ -2,40 +2,64 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+//first approach
+// void sort012(int *arr, int n)
+// {
+//     int c0 = 0, c1 = 0, c2 = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == 0)
+//             c0++;
+//         if (arr[i] == 1)
+//             c1++;
+//         if (arr[i] == 2)
+//             c2++;
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (i >= 0 && i < c0)
+//             arr[i] = 0;
+//         else if (i >= c0 && i < c0 + c1)
+//             arr[i] = 1;
+//         else
+//         {
+//             arr[i] = 2;
+//         }
+//     }
+// }
 void sort012(int *arr, int n)
 {
-    int c0 = 0, c1 = 0, c2 = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] == 0)
-            c0++;
-        if (arr[i] == 1)
-            c1++;
-        if (arr[i] == 2)
-            c2++;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        if (i >= 0 && i < c0)
-            arr[i] = 0;
-        else if (i >= c0 && i < c0 + c1)
-            arr[i] = 1;
-        else
-        {
-            arr[i] = 2;
+    int low=0;
+    int mid=0;
+    int high=n-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(arr[mid],arr[low]);
+            mid++;
+            low++;
+        }
+        else if(arr[mid]==1){
+            mid++;
+        }
+        
+        else{
+            swap(arr[mid],arr[high]);
+            high--;
         }
     }
+
 }
+//DNf(Dutch National Flag ) Algo.
 int main()
 {
     int arr[] = {2, 1, 0, 2, 2, 2, 1, 1, 0, 0, 0};
-    for (int i = 1; i < 11; i++)
+    for (int i = 0; i < 11; i++)
     {
         cout << arr[i] << " ";
     }
     cout << endl;
     sort012(arr, 11);
-    for (int i = 1; i < 11; i++)
+    for (int i = 0; i < 11; i++)
     {
         cout << arr[i] << " ";
     }
