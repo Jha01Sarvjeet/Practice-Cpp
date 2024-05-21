2b5377534c9704e40db4a59f26b01611021b98a2
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-05-21 12:25:38 +0530

    sort array of 0 ,1  and 2

--- 10_array/16_sort012.cpp
+++ 10_array/16_sort012.cpp
@@ -1,40 +1,44 @@
-//sort the array of 0 ,1 and 2 without using sort functoin
-#include<iostream>
-#include <bits/stdc++.h> 
+// sort the array of 0 ,1 and 2 without using sort functoin
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void sort012(int *arr,int n){
-    int c0=0,c1=0,c2=0;
-    for (int i=0;i<n;i++){
-        if(arr[i]==0)c0++;
-        if(arr[i]==1)c1++;
-        if(arr[i]==2)c2++;
+void sort012(int *arr, int n)
+{
+    int c0 = 0, c1 = 0, c2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 0)
+            c0++;
+        if (arr[i] == 1)
+            c1++;
+        if (arr[i] == 2)
+            c2++;
     }
-    for(int i=0;i<n;i++){
-        if(i>=0 && i<c0)
-            arr[i]=0;
-        else  if(i>=c0 && i<c0+c1)
-            arr[i]=1;
-        else{
-            arr[i]=2;
+    for (int i = 0; i < n; i++)
+    {
+        if (i >= 0 && i < c0)
+            arr[i] = 0;
+        else if (i >= c0 && i < c0 + c1)
+            arr[i] = 1;
+        else
+        {
+            arr[i] = 2;
         }
-
+    }
+}
+int main()
+{
+    int arr[] = {2, 1, 0, 2, 2, 2, 1, 1, 0, 0, 0};
+    for (int i = 1; i < 11; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    sort012(arr, 11);
+    for (int i = 1; i < 11; i++)
+    {
+        cout << arr[i] << " ";
     }
 
-    
-}
-int main(){
-int arr[]={2,1,0,2,2,2,1,1,0,0,0};
-for(int i=1;i<11;i++){
-    cout<<arr[i]<< " ";
-
-}
-cout<<endl;
-sort012(arr,11);
-for(int i=1;i<11;i++){
-    cout<<arr[i]<< " ";
-
-}
-
-    
-     return 0;
+    return 0;
 }
