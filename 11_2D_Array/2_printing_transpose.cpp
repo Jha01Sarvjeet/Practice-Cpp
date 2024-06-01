4c0f0eefb289de7e4308cc09b06b23f23e56e244
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-01 10:52:11 +0530

    updated , to transpose the array

--- 11_2D_Array/2_printing_transpose.cpp
+++ 11_2D_Array/2_printing_transpose.cpp
@@ -5,9 +5,17 @@ using namespace std;
 
 int main(){
 int arr[2][3]={1,2,3,4,5,6};
+int trans[3][2];
+
 for(int i=0;i<3;i++){
     for(int j=0;j<2;j++){
-        cout<<arr[j][i]<<" ";
+       trans[i][j]=arr[j][i];
+    }
+    
+}
+for(int i=0;i<3;i++){
+    for(int j=0;j<2;j++){
+       cout<<trans[i][j]<<" ";
     }
     cout<<endl;
 }
