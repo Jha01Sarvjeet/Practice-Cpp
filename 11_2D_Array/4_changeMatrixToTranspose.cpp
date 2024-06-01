88affa3d76bd6a607fcd900821bbfb5c060110aa
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-01 11:31:36 +0530

    updated , transform the matrix to it's transpose in the same array

--- 11_2D_Array/4_changeMatrixToTranspose.cpp
+++ 11_2D_Array/4_changeMatrixToTranspose.cpp
@@ -6,15 +6,23 @@ using namespace std;
 int main(){
 int arr[4][4]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 
-
 for(int i=0;i<4;i++){
-    for(int j=0;j<4;j++){
-       if(i<j){
+    for(int j=i+1;j<4;j++){
+      
         swap(arr[j][i],arr[i][j]);
-       }
+       
     }
     
 }
+//we can use this also
+// for(int i=0;i<4;i++){
+//     for(int j=0;j<4;j++){
+//        if(i<j){
+//         swap(arr[j][i],arr[i][j]);
+//        }
+//     }
+    
+// }
 for(int i=0;i<4;i++){
     for(int j=0;j<4;j++){
       cout<<arr[i][j]<<" ";
