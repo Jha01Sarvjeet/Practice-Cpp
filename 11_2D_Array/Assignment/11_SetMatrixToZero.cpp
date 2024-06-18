7cbdfe92f62970f97b0c1bdca8943fbd56e56696
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-18 21:35:39 +0530

    set matrix to zero with better approach

--- 11_2D_Array/Assignment/11_SetMatrixToZero.cpp
+++ 11_2D_Array/Assignment/11_SetMatrixToZero.cpp
@@ -12,52 +12,150 @@ int main()
     cout << "enter number of column" << endl;
     cin >> m;
     int arr[n][m];
-    int temp[n][m];
-    // enter element in array
+
+    // 1)Brute force approach  with extraspace
+
+    // int temp[n][m];
+
+    // // enter element in array
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         cin >> arr[i][j];
+    //         temp[i][j]=arr[i][j];
+    //     }
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         cout << temp[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // // finding zero and converting corresponding row and column to zero
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         if (arr[i][j] == 0)
+    //         {
+    //             // converting corresponding row to zero
+    //             for (int k = 0; k < m; k++)
+    //             {
+    //                 temp[i][k] = 0;
+    //             }
+    //              // converting corresponding column to zero
+    //         for (int l = 0; l < n; l++)
+    //         {
+    //             temp[l][j] = 0;
+    //         }
+    //         }
+
+    //     }
+    // }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         cout << temp[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 2)brute force approach
+    //  for (int i = 0; i < n; i++)
+    //  {
+    //      for (int j = 0; j < m; j++)
+    //      {
+    //          cin >> arr[i][j];
+
+    //     }
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         if (arr[i][j] == 0)
+    //         {
+    //             // converting corresponding row to zero
+    //             for (int k = 0; k < m; k++)
+    //             {
+    //                if (arr[i][k] != 0)
+    //                     arr[i][k] = -1;
+    //             }
+    //              // converting corresponding column to zero
+    //         for (int l = 0; l < n; l++)
+    //         {
+    //            if (arr[l][j] != 0)
+    //                     arr[l][j] = -1;
+    //         }
+    //         }
+
+    //     }
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //      if(arr[i][j]==-1){
+    //         arr[i][j]=0;
+    //      }
+
+    //     }
+    // }
+    //  for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         cout << arr[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // Better approach
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             cin >> arr[i][j];
-            temp[i][j]=arr[i][j];
+        }
+    }
+    int row[n]={0};
+    int col[m]={0};
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if(arr[i][j]==0){
+                row[i]=1;
+                col[j]=1;
+            }
         }
     }
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            cout << temp[i][j] << " ";
+            if((row[i]==1)|| col[j]==1){
+                        arr[i][j]=0;
+            }
+        }
+    }
+
+     for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
         }
         cout << endl;
     }
-    // finding zero and converting corresponding row and column to zero
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (arr[i][j] == 0)
-            {
-                // converting corresponding row to zero
-                for (int k = 0; k < m; k++)
-                {
-                    temp[i][k] = 0;
-                }
-                 // converting corresponding column to zero
-            for (int l = 0; l < n; l++)
-            {
-                temp[l][j] = 0;
-            }
-            }
-           
-        }
-    }
-   
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cout << temp[i][j] << " ";
-        }
-        cout << endl;
-    }
+    
+
+
 }
