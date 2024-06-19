abee24a4678ecb26a8a7f96196debd74d0e8a049
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-19 11:58:09 +0530

    updated with optimal solution

--- 11_2D_Array/Assignment/11_SetMatrixToZero.cpp
+++ 11_2D_Array/Assignment/11_SetMatrixToZero.cpp
@@ -117,37 +117,91 @@ int main()
 
     // Better approach
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> arr[i][j];
-        }
-    }
-    int row[n]={0};
-    int col[m]={0};
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         cin >> arr[i][j];
+    //     }
+    // }
+    // int row[n]={0};
+    // int col[m]={0};
     
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if(arr[i][j]==0){
-                row[i]=1;
-                col[j]=1;
-            }
-        }
-    }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if((row[i]==1)|| col[j]==1){
-                        arr[i][j]=0;
-            }
-        }
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         if(arr[i][j]==0){
+    //             row[i]=1;
+    //             col[j]=1;
+    //         }
+    //     }
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         if((row[i]==1)|| col[j]==1){
+    //                     arr[i][j]=0;
+    //         }
+    //     }
+    // }
 
-     for (int i = 0; i < n; i++)
+    //  for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         cout << arr[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    
+//OPtimal approah
+for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        cin>>arr[i][j];
+    }
+}
+int col0=1;
+for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        if(arr[i][j]==0){
+            arr[i][0]=0;
+            if(j!=0){
+                arr[0][j]=0;
+            }
+            else{
+              col0=0;  
+            }
+        }
+    }
+}
+for(int i=1;i<n;i++){
+    for(int j=1;j<m;j++){
+        if(arr[i][0]==0){
+            for(int k=0;k<m;k++){
+                arr[i][k]=0;
+            }
+        }
+        if(arr[0][j]==0){
+            for(int k=0;k<n;k++){
+                arr[k][j]=0;
+            }
+        }
+
+    }
+}
+if(arr[0][0]==0){
+     for(int k=0;k<m;k++){
+                arr[0][k]=0;
+            }
+}
+if(col0==0){
+    for(int k=0;k<n;k++){
+                arr[k][0]=0;
+            }
+}
+ for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
@@ -155,7 +209,5 @@ int main()
         }
         cout << endl;
     }
-    
-
 
 }
