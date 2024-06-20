7be82b4941f556e22475635a3c43f6ead2f6fba2
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-20 12:41:17 +0530

    built in  function to claculate number of character in string

---
+++ 12_String/4_stringBuiltInFunction.cpp
@@ -0,0 +1,14 @@
+//Built IN function String
+#include<iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+int main(){
+ string str="hello dear";
+ //to find number of character of string
+
+ int len1=str.length();
+ int len2=str.size();
+ cout<<len1<<" "<<len2<<endl;
+    
+     return 0;
+}
