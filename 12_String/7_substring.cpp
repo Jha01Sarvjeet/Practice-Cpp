ef2288b69fe17dac778046ed63c25163bf6b3a56
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-20 21:04:27 +0530

    to get the substring of string

---
+++ 12_String/7_substring.cpp
@@ -0,0 +1,11 @@
+//to print the sunstring of string 
+//<string name>.substr(idx):- from idex to end
+#include<iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+int main(){
+string str="hello";
+cout<<str.substr(1); //from 1 index to end
+    
+     return 0;
+}
