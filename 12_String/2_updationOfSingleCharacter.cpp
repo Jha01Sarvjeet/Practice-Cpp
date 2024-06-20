f84318681fc0d2d05e8f544580b3a15dd940ea16
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-20 12:21:06 +0530

    update single character of string

---
+++ 12_String/2_updationOfSingleCharacter.cpp
@@ -0,0 +1,11 @@
+//to update single character of string
+#include<iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+int main(){
+string str="hello";
+str[0]='s';
+cout<<str<<endl;
+    
+     return 0;
+}
