524b8661e68330a748ae060ea8c803f8679a2352
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-20 12:46:59 +0530

    push_back() and pop_back() in string

--- 12_String/4_stringBuiltInFunction.cpp
+++ 12_String/4_stringBuiltInFunction.cpp
@@ -4,11 +4,20 @@
 using namespace std;
 int main(){
  string str="hello dear";
- //to find number of character of string
+ //to find number of character of string   <strname>.length().<strname>.size()
 
- int len1=str.length();
- int len2=str.size();
- cout<<len1<<" "<<len2<<endl;
-    
+//  int len1=str.length();
+//  int len2=str.size();
+//  cout<<len1<<" "<<len2<<endl;
+
+
+//<strname>.push_back() to add single character at end 
+    // cout<<str<<endl;
+    // str.push_back( '!');
+    // cout<<str<<endl;
+//<strname>.pop_back() to remove single character from end 
+    // cout<<str<<endl;
+    // str.pop_back();
+    // cout<<str<<endl;
      return 0;
 }
