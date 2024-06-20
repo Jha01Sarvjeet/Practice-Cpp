8b5db750a6b6e8208eeb08d4ada2041dfc9aa1cb
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-20 21:07:13 +0530

    updated to find substring pf specific length from a given index

--- 12_String/7_substring.cpp
+++ 12_String/7_substring.cpp
@@ -1,11 +1,13 @@
 //to print the sunstring of string 
 //<string name>.substr(idx):- from idex to end
+//<string name>.substr(idx,len):- from idex till length of the string 
 #include<iostream>
 #include <bits/stdc++.h> 
 using namespace std;
 int main(){
 string str="hello";
-cout<<str.substr(1); //from 1 index to end
+cout<<str.substr(1)<<endl; //from 1 index to end
+cout<<str.substr(1,2); //from 1 index kof length 2
     
      return 0;
 }
