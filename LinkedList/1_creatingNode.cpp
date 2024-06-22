3b3d960675a6a7046d747cf7c6d0bd00f4cefb24
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-22 16:31:27 +0530

    add function to add node at head

--- LinkedList/1_creatingNode.cpp
+++ LinkedList/1_creatingNode.cpp
@@ -8,46 +8,62 @@ public:
     int data;
     Node *next;
 
-    //constructing constructer
-    Node(int value){
-        this->data=value;
-        this->next=NULL;
+    // constructing constructer
+    Node(int value)
+    {
+        this->data = value;
+        this->next = NULL;
     }
 };
 
-// creating single ll 
-//insert at tail
-void insertAtTail(Node * &head,int data){
-    Node * node=new Node(data);
-    Node * temp=head;
-    while(temp->next!=NULL){
-        temp =temp->next;
+// creating single ll
+// insert at tail
+void insertAtTail(Node *&head, int data)
+{
+    Node *node = new Node(data);
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
     }
-    temp->next=node;
-
+    temp->next = node;
 }
-void print(Node * head){
-    Node *temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+// insert at head
+void insertAtHead(Node *&head, int data)
+{
+    Node *node = new Node(data);
+    
+    node->next = head;
+    head=node;
+}
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
 
-    cout<<endl;
+    cout << endl;
 }
-
-
 
 int main()
 {
     Node *node1 = new Node(5);
     print(node1);
-    insertAtTail(node1,10);
-      print(node1);
-      insertAtTail(node1,25);
-      print(node1);
- insertAtTail(node1,50);
-      print(node1);
+    insertAtTail(node1, 10);
+    print(node1);
+    insertAtTail(node1, 25);
+    print(node1);
+    insertAtTail(node1, 50);
+    print(node1);
+    insertAtHead(node1,69);
+
+    print(node1);
+    insertAtHead(node1,96);
+
+    print(node1);
 
     return 0;
 }
