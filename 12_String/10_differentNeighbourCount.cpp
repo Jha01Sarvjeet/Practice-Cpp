d8b6689b461aca5498c1e81bf71d557b4d6aa111
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-03 14:55:27 +0530

    count the number of chara cter with diffrent neighbour

---
+++ 12_String/10_differentNeighbourCount.cpp
@@ -0,0 +1,39 @@
+// to count the number whose neighbour character are diffrent
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str;
+    cout << "enter string" << endl;
+    cin >> str;
+    int c = 0;
+    if (str.length() == 1)
+    {
+        cout << "the total number of different neighbour are " << c << endl;
+    }
+    else
+    {
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (i == 0)
+            {
+                if (str[i] != str[i + 1])
+                    c++;
+            }
+            else if (i == str.length() - 1)
+            {
+                if (str[i] != str[i - 1])
+                    c++;
+            }
+            else if ((str[i] != str[i - 1]) && (str[i] != str[i + 1]))
+            {
+                c++;
+            }
+        }
+
+        cout << "the total number of different neighbour are " << c << endl;
+    }
+    return 0;
+}
