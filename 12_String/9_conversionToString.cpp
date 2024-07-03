96bd96edc485bed665a8c82428da9de61121ddd2
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-03 14:18:52 +0530

    conversion to string

---
+++ 12_String/9_conversionToString.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <bits/stdc++.h> 
+//to convert into string
+using namespace std;
+int main(){
+int a=9846464;
+cout<<a<<endl;
+string str=to_string(a);
+
+cout<<str<<endl;
+cout<<str.length()<<endl;
+
+
+
+    
+     return 0;
+}
