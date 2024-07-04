11e0ecfb9faf0b4140b064d3639cc20fe04810b1
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-04 14:49:41 +0530

    updated with better solution

--- 12_String/13_characterOcuuringMost.cpp
+++ 12_String/13_characterOcuuringMost.cpp
@@ -9,39 +9,60 @@ int main()
      cin >> str;
      int n = str.length();
      int max = 0;
-     char ch;
-     for (int i = 0; i < n; i++)
-     {
-          int count = 1;
-          for (int j = i + 1; j < n; j++)
-          {
-               if (str[j] == str[i])
-               {
-                    count++;
-               }
-          }
-          if (count > max)
-          {
-               max = count;
-               ch=str[i];
-          }
+//Brute force approach
+
+     // char ch;
+     // for (int i = 0; i < n; i++)
+     // {
+     //      int count = 1;
+     //      for (int j = i + 1; j < n; j++)
+     //      {
+     //           if (str[j] == str[i])
+     //           {
+     //                count++;
+     //           }
+     //      }
+     //      if (count > max)
+     //      {
+     //           max = count;
+     //           ch=str[i];
+     //      }
+     // }
+     // for (int i = 0; i < n; i++)
+     // {
+     //      int count = 1;
+     //      for (int j = i + 1; j < n; j++)
+     //      {
+     //           if (str[j] == str[i])
+     //           {
+     //                count++;
+     //           }
+     //      }
+     //      if (count == max)
+     //      {
+     //           cout<<"character having maximum count := "<<str[i]<<" and having count "<<count<<endl;
+     //      }
+     // }
+     
+//Better approach
+vector<int>track(26,0);
+for (int i=0;i<n;i++){
+     int ascii=str[i]-'a';
+     track[ascii]++;
+}
+
+for(int i=0;i<26;i++){
+     if(track[i]>max){
+          max=track[i];
      }
-     for (int i = 0; i < n; i++)
-     {
-          int count = 1;
-          for (int j = i + 1; j < n; j++)
-          {
-               if (str[j] == str[i])
-               {
-                    count++;
-               }
-          }
-          if (count == max)
-          {
-               cout<<"character having maximum count := "<<str[i]<<" and having count "<<count<<endl;
-          }
+}
+for(int i=0;i<26;i++){
+     if(track[i]==max){
+          int ascii=i+97;
+          char ch=char(ascii);
+            cout<<"character having maximum count := "<<ch<<" and having count "<<max<<endl;
      }
-     // cout<<"character having maximum count := "<<ch<<" and having count "<<max<<endl;
+}
 
      return 0;
 }
