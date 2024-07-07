75f1823ca3b649acdec17df5147afef256d60763
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-08 00:12:24 +0530

    updated

--- 12_String/String Stream/2_printWordWithMaxcount.cpp
+++ 12_String/String Stream/2_printWordWithMaxcount.cpp
@@ -9,20 +9,44 @@ string str;
 cout<<"enter string"<<endl;
 getline(cin,str);//to get input cintaining space
 stringstream s(str);
-map<string ,int>mpp;
+//solved using map
+// map<string ,int>mpp;
+// string temp;
+// while(s>>temp){
+//     mpp[temp]++;
+// }
+// int maxFreq=0;
+// string st;
+// for(auto i :mpp){
+//     if(i.second>maxFreq){
+//         maxFreq=i.second;
+//         st=i.first;
+//     }
+// }
+// cout<<"words having maximum count is \" "<<st<<" \"and count is "<<maxFreq<<endl; 
+
+
+//solving usiing vector
+vector<string >vec;
 string temp;
 while(s>>temp){
-    mpp[temp]++;
+    vec.push_back(temp);
 }
-int maxFreq=0;
-string st;
-for(auto i :mpp){
-    if(i.second>maxFreq){
-        maxFreq=i.second;
-        st=i.first;
+sort(vec.begin(),vec.end());
+int maxi=0;
+int count=1;
+for(int i=0;i<vec.size();i++){
+    if(vec[i]==vec[i+1]){
+        count++;
+        maxi=max(count,maxi);
+
+    }
+    else{
+      count=1;  
     }
 }
-cout<<"words having maximum count is \" "<<st<<" \"and count is "<<maxFreq<<endl; 
+cout<<"words having maximum count is" <<maxi<<endl; 
+
     
      return 0;
 }
