a3c48b43af9ccad4ac743010be85858a8a3c41af
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-08 00:19:28 +0530

    stoi to convert string into integer

---
+++ 12_String/14_stoi_stoll.cpp
@@ -0,0 +1,14 @@
+//stoi & stoll
+
+#include<iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+int main(){
+
+//to convert string into integer
+string str="123456";
+int num=stoi(str);
+cout<<num<<endl;
+    
+     return 0;
+}
