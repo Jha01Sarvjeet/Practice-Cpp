20d451cc684f09675649455000259d192ae39b6a
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-08 00:21:10 +0530

    stoll to convert string into long long

--- 12_String/14_stoi_stoll.cpp
+++ 12_String/14_stoi_stoll.cpp
@@ -6,8 +6,15 @@ using namespace std;
 int main(){
 
 //to convert string into integer
-string str="123456";
-int num=stoi(str);
+// string str="123456";
+// int num=stoi(str);
+// cout<<num<<endl;
+    
+//      return 0;
+
+//to convert string into long long
+string str="123456684684684";
+long long num=stoll(str);
 cout<<num<<endl;
     
      return 0;
