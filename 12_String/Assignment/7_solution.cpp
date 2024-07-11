6499d8ada77c00bdd7c56fad1720cb69534f6123
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-11 19:57:22 +0530

    second largest digit

--- 12_String/Assignment/7_solution.cpp
+++ 12_String/Assignment/7_solution.cpp
@@ -22,3 +22,20 @@ cout<<" Second largest"<<slarge<<endl;
     
      return 0;
 }
+
+/*#include <bits/stdc++.h>
+using namespace std;
+int main() {
+string p;
+cin >> p;
+char f = '?', s = '?';
+for (auto &i : p) {
+if (f == '?' || i > f) {
+s = f;
+f = i;
+} else if (i > s) {
+s = i;
+}
+}
+cout << s;
+}*/
