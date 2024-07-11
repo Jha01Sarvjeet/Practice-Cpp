e39c1162442e416c5c62ef1b9e4cd62c70aa5786
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-11 19:48:32 +0530

    second largest digit

---
+++ 12_String/Assignment/7_solution.cpp
@@ -0,0 +1,24 @@
+// Find the second largest digit in the string consisting of digits from ‘0’ to ‘9’.
+#include<iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+int main(){
+string str;
+cout<<"enter string"<<endl;
+cin>>str;
+int flarge=INT_MIN;
+int slarge=INT_MIN;
+for (int i=0;i<str.length();i++){
+    if((str[i]-'0')>flarge){
+            slarge=flarge;
+            flarge=str[i]-'0';
+        
+    }
+    else if((str[i]-'0')<flarge && (str[i]-'0')>slarge){
+        slarge=(int)str[i]-'0';
+    }
+}
+cout<<" Second largest"<<slarge<<endl;
+    
+     return 0;
+}
