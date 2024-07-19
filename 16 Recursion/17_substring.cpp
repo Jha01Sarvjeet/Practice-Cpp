d90c055b40ca2c7e1d5d7fbb57d4fecec1d5e753
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-19 15:59:12 +0530

    updated substring of string

--- 16 Recursion/17_substring.cpp
+++ 16 Recursion/17_substring.cpp
@@ -2,19 +2,33 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-void substringOfString(string answer, string original)
+// void substringOfString(string answer, string original)
+// {
+//     if (original.length() == 0)
+//     {
+//         cout << answer << endl;
+//         return;
+//     }
+//     char ch = original[0];
+//     // substringOfString(answer + ch, original.substr(1));
+//     // substringOfString(answer, original.substr(1));
+
+//     substringOfString(answer, original.substr(1));
+//     substringOfString(answer + ch, original.substr(1));
+// }
+void substringOfString(string answer, string original,vector<string>&vec)
 {
     if (original.length() == 0)
     {
-        cout << answer << endl;
+        vec.push_back(answer);
         return;
     }
     char ch = original[0];
     // substringOfString(answer + ch, original.substr(1));
     // substringOfString(answer, original.substr(1));
 
-    substringOfString(answer, original.substr(1));
-    substringOfString(answer + ch, original.substr(1));
+    substringOfString(answer, original.substr(1),vec);
+    substringOfString(answer + ch, original.substr(1),vec);
 }
 int main()
 {
@@ -22,7 +36,15 @@ int main()
     cout << "enter string" << endl;
     getline(cin, str);
     cout << '\n';
-    substringOfString("", str);
+    vector<string>vec;
+    // substringOfString("", str);
+
+    substringOfString("", str,vec);
+    for(auto i :vec){
+        cout<<i<< " ";
+    }
+
+
 
     return 0;
 }
