e2641d03e44c7db9dc4ab57d69879d25c5454c9c
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-23 10:38:09 +0530

    sub array using recursion

--- 16 Recursion/21_subArray.cpp
+++ 16 Recursion/21_subArray.cpp
@@ -1,33 +1,62 @@
 // to print the subarray
 
 // iterative approach
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
-void print(int *arr, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-int main()
-{
-    int arr[] = {1, 2, 3};
-    for (int i = 0; i < 3; i++)
-    {
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print(int *arr, int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
+// int main()
+// {
+//     int arr[] = {1, 2, 3};
+//     for (int i = 0; i < 3; i++)
+//     {
 
-        for (int j = i; j < 3; j++)
-        {
+//         for (int j = i; j < 3; j++)
+//         {
             
-            for (int k = i; k <= j; k++)
-            {
-                cout << arr[k] << " ";
-            }
-            cout << endl;
-        }
-    }
+//             for (int k = i; k <= j; k++)
+//             {
+//                 cout << arr[k] << " ";
+//             }
+//             cout << endl;
+//         }
+//     }
 
-    return 0;
+//     return 0;
+// }
+
+
+//recursive approach
+
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std; 
+void subArray(int *arr,int i,int n){
+    if(n==i){
+        return;
+    }
+    for(int j=i;j<n;j++){
+        for(int k=i;k<=j;k++){
+            cout<<arr[k]<<" ";
+        }
+          cout<<endl;
+    }
+  
+    subArray(arr,i+1,n);
+
+
+}
+
+int main(){
+int arr[]={1,2,3};
+subArray(arr,0,3);
+    
+     return 0;
 }
