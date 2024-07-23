379c871d9268c21541774b3b372115740a089dcc
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-23 11:48:46 +0530

    to find gcd using recursion

--- 16 Recursion/23_GCD.cpp
+++ 16 Recursion/23_GCD.cpp
@@ -1,14 +1,41 @@
 //to print the Gcd of two number
+// #include<iostream>
+// #include <bits/stdc++.h> 
+// using namespace std;
+// void Gcd(int a,int b){
+//     for(int i=min(a,b);i>=1;i--){
+//         if(a%i==0 && b%i==0){
+//             cout<<i<<endl;
+//             break;
+            
+//         }
+//     }
+// }
+// int main(){
+// int a,b;
+// cout<<"enter a,b"<<endl;
+// cin >>a;
+// cin>>b;
+// Gcd(a,b);
+    
+//      return 0;
+// }
+
+//using iteration
 #include<iostream>
 #include <bits/stdc++.h> 
 using namespace std;
 void Gcd(int a,int b){
-    for(int i=min(a,b);i>=1;i--){
-        if(a%i==0 && b%i==0){
-            cout<<i<<endl;
-            break;
-            
-        }
+    if(a==0){
+        cout<<b<<endl;
+        return;
+    }
+    else if(b==0){
+        cout<<a<<endl;
+        return;
+    }
+    else{
+         Gcd(b%a,a);
     }
 }
 int main(){
