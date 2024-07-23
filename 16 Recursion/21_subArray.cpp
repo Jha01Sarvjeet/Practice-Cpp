30f6285b956b4c50c611c017a936bf6f069615a3
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-23 11:08:28 +0530

    sub array using recursion with other approach

--- 16 Recursion/21_subArray.cpp
+++ 16 Recursion/21_subArray.cpp
@@ -33,30 +33,59 @@
 // }
 
 
-//recursive approach
+//first recursive approach
+
+// #include<iostream>
+// #include <bits/stdc++.h>
+// using namespace std; 
+// void subArray(int *arr,int i,int n){
+//     if(n==i){
+//         return;
+//     }
+//     for(int j=i;j<n;j++){
+//         for(int k=i;k<j;k++){
+//             cout<<arr[k]<<" ";
+//         }
+//           cout<<endl;
+//     }
+  
+//     subArray(arr,i+1,n);
+
+
+// }
+
+// int main(){
+// int arr[]={1,2,3};
+// subArray(arr,0,3);
+    
+//      return 0;
+// }
+
+//second recursive approch
 
 #include<iostream>
-#include <bits/stdc++.h>
-using namespace std; 
-void subArray(int *arr,int i,int n){
-    if(n==i){
+#include <bits/stdc++.h> 
+using namespace std;
+void disaplay(vector<int>ans){
+    for(auto i :ans){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+void subArray(vector<int>ans,vector<int>original,int idx){
+    if(idx==original.size()){
+        disaplay(ans);
         return;
     }
-    for(int j=i;j<n;j++){
-        for(int k=i;k<=j;k++){
-            cout<<arr[k]<<" ";
-        }
-          cout<<endl;
+    subArray(ans,original,idx+1);
+    if(ans.size()==0||ans[ans.size()-1]==original[idx-1]){
+        ans.push_back(original[idx]);
+        subArray(ans,original,idx+1);
     }
-  
-    subArray(arr,i+1,n);
-
-
 }
-
 int main(){
-int arr[]={1,2,3};
-subArray(arr,0,3);
+   vector<int>vec{1,2,3,4};
+    subArray({},vec,0);
     
      return 0;
 }
