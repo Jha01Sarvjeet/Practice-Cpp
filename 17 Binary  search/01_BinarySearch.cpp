44c94ba22c9ce05f873a18251e49e67773868eec
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-08-06 09:18:11 +0530

    updated with recursive approach

--- 17 Binary  search/01_BinarySearch.cpp
+++ 17 Binary  search/01_BinarySearch.cpp
@@ -2,19 +2,34 @@
 #include<iostream>
 #include <bits/stdc++.h> 
 using namespace std;
+//iterative approach
+
+// int binarySearch(int arr[],int s,int e,int target){
+//     int mid=s+(e-s)/2;
+//     while(s<=e){
+//         if(arr[mid]==target)return mid;
+//         else if(arr[mid]>target)e=mid-1;
+//         else s=mid+1;
+//         mid=s+(e-s)/2;
+//     }
+//     return -1;
+// }
+
+//recursive approach
+
 int binarySearch(int arr[],int s,int e,int target){
     int mid=s+(e-s)/2;
-    while(s<=e){
-        if(arr[mid]==target)return mid;
-        else if(arr[mid]>target)e=mid-1;
-        else s=mid+1;
-        mid=s+(e-s)/2;
+    if(s>e)return -1;
+    if(arr[mid]==target)return mid;
+    else if(arr[mid]>target)
+        return binarySearch(arr,s,mid-1,target);
+    else{
+        return binarySearch(arr,mid+1,e,target);
     }
-    return -1;
 }
 int main(){
-int arr[]={1,2,3,5,6,9,7};
-int ans=binarySearch(arr,0,6,-5);
+int arr[]={1,2,3,5,6,9,11};
+int ans=binarySearch(arr,0,6,11);
 cout<<ans<<endl;
     
      return 0;
