6059cf5123668832d9d0a2bf8f7ed87e609076c8
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-08-06 11:39:09 +0530

    first occurance

--- 17 Binary  search/04_firstoccurance.cpp
+++ 17 Binary  search/04_firstoccurance.cpp
@@ -3,15 +3,32 @@
 #include <bits/stdc++.h> 
 using namespace std;
 // iterative approach
-int firstOccurance(int *arr,int target,int n){
-    for(int i=0;i<n;i++){
-        if(arr[i]==target)return i;
+// int firstOccurance(int *arr,int target,int n){
+//     for(int i=0;i<n;i++){
+//         if(arr[i]==target)return i;
+//     }
+//     return -1;
+// }
+
+//recursive solution
+int firstOccurance(int *arr,int target,int s,int e,int idx ){
+    int mid=s+(e-s)/2;
+    if(s>e)return idx;
+    else if(arr[mid]==target){
+        idx=mid;
+        firstOccurance( arr, target,s, mid-1, idx );
     }
-    return -1;
+    else if(arr[mid]<target){
+            firstOccurance( arr, target,mid+1, e, idx );
+    }
+    else{
+     firstOccurance( arr, target,s, mid-1, idx );
+    }
+    
 }
 int main(){
-int arr[]={1,2,3,3,3,4,5,6,7};
-cout<<firstOccurance(arr,3,8);
+int arr[]={1,3,3,3,3,4,5,6,7};
+cout<<firstOccurance(arr,7,0,8,INT_MAX);
     
      return 0;
 }
