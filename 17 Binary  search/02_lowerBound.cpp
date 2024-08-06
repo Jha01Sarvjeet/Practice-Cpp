8f236574cfc5aaaa321155505a6b64d8bb1b28b2
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-08-06 09:50:04 +0530

    updated with recursive slution

--- 17 Binary  search/02_lowerBound.cpp
+++ 17 Binary  search/02_lowerBound.cpp
@@ -2,33 +2,52 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-int lowerBound(int *arr, int s, int e, int target)
+// iterative approach
+// int lowerBound(int *arr, int s, int e, int target)
+// {
+//     int mid = s + (e - s) / 2;
+
+//     while (s <= e)
+//     {
+//         if (arr[mid] == target)
+//         {
+
+//             return arr[mid-1];
+//         }
+//         else if (arr[mid] < target)
+//         {
+
+//             s=mid+1;
+//         }
+//         else{
+//             e=mid-1;
+//         }
+//         mid = s + (e - s) / 2;
+//     }
+//     return arr[e];
+// }
+
+// recursive approach
+
+int lowerBound(int *arr, int s, int e, int target, int ans)
 {
     int mid = s + (e - s) / 2;
-    
-    while (s <= e)
+    if(s>e)return ans;
+    if (arr[mid] == target)
+        return arr[mid - 1];
+    else if (arr[mid] < target)
     {
-        if (arr[mid] == target)
-        {
-           
-            return arr[mid-1];
-        }
-        else if (arr[mid] < target)
-        {
-            
-            s=mid+1;
-        }
-        else{
-            e=mid-1;
-        }
-        mid = s + (e - s) / 2;
+        ans = max(ans, arr[mid]);
+        lowerBound(arr, mid + 1, e, target, ans);
     }
-    return arr[e];
+    else{
+        lowerBound(arr, s, mid-1, target, ans);
+    }
 }
 int main()
 {
-    int arr[]={1,5,9,10,15};
-    cout<<lowerBound(arr,0,4,7);
+    int arr[] = {1, 5, 9, 10, 15};
+    cout << lowerBound(arr, 0, 4, 10252,0);
 
     return 0;
 }
