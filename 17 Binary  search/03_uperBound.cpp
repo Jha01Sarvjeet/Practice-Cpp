1c0cb7f2a341231145ab1ed23c2efa54b1b84217
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-08-06 10:42:00 +0530

    updated with recursive solution

--- 17 Binary  search/03_uperBound.cpp
+++ 17 Binary  search/03_uperBound.cpp
@@ -3,27 +3,45 @@
 #include <bits/stdc++.h> 
 using namespace std;
 //iterativ approach
-int upperBound(int *arr,int s,int e,int target){
-    int mid=s+(e-s)/2;
+// int upperBound(int *arr,int s,int e,int target){
+//     int mid=s+(e-s)/2;
 
-    while(s<=e){
-        if(arr[mid]==target)return arr[mid+1];
-        else if(arr[mid]>target){
-            e=mid-1;
-        }
-        else{
-            s=mid+1;
-        }
-        mid=s+(e-s)/2;
+//     while(s<=e){
+//         if(arr[mid]==target)return arr[mid+1];
+//         else if(arr[mid]>target){
+//             e=mid-1;
+//         }
+//         else{
+//             s=mid+1;
+//         }
+//         mid=s+(e-s)/2;
 
+//     }
+//     return arr[s];
+
+// }
+
+//recursive solution
+int upperBound(int *arr, int s, int e, int target, int ans)
+{
+    int mid = s + (e - s) / 2;
+    if(s>e)return ans;
+    if (arr[mid] == target)
+        return arr[mid + 1];
+    else if (arr[mid] < target)
+    {
+        
+        upperBound(arr, mid + 1, e, target, ans);
     }
-    return arr[s];
-
+    else{
+        ans = min(ans, arr[mid]);
+        upperBound(arr, s, mid-1, target, ans);
+    }
 }
 int main(){
 
     int arr[] = {1, 5, 9, 10, 15};
-    cout << upperBound(arr, 0, 4, 6);
+    cout << upperBound(arr, 0, 4,6,INT_MAX);
 
      return 0;
 }
