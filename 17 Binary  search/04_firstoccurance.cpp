363d706dd4c76019cd38cab49f091359f9ce01be
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-08-06 11:44:30 +0530

    iterative solution updates:

--- 17 Binary  search/04_firstoccurance.cpp
+++ 17 Binary  search/04_firstoccurance.cpp
@@ -1,34 +1,58 @@
-//to find the position of first occurance of the number
-#include<iostream>
-#include <bits/stdc++.h> 
+// to find the position of first occurance of the number
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 // iterative approach
-// int firstOccurance(int *arr,int target,int n){
-//     for(int i=0;i<n;i++){
-//         if(arr[i]==target)return i;
-//     }
-//     return -1;
-// }
-
-//recursive solution
-int firstOccurance(int *arr,int target,int s,int e,int idx ){
-    int mid=s+(e-s)/2;
-    if(s>e)return idx;
-    else if(arr[mid]==target){
-        idx=mid;
-        firstOccurance( arr, target,s, mid-1, idx );
+int firstOccurance(int *arr, int target, int n)
+{
+    int s = 0;
+    int e = n - 1;
+    int mid = (s + e) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (arr[mid] == target)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (arr[mid] > target)
+            e = mid - 1;
+        else
+        {
+            s = mid + 1;
+        }
+        mid = (s + e) / 2;
     }
-    else if(arr[mid]<target){
-            firstOccurance( arr, target,mid+1, e, idx );
-    }
-    else{
-     firstOccurance( arr, target,s, mid-1, idx );
-    }
-    
 }
-int main(){
-int arr[]={1,3,3,3,3,4,5,6,7};
-cout<<firstOccurance(arr,7,0,8,INT_MAX);
-    
-     return 0;
+
+// recursive solution
+int firstOccurance(int *arr, int target, int s, int e, int idx)
+{
+    int mid = s + (e - s) / 2;
+    if (s > e)
+        return idx;
+    else if (arr[mid] == target)
+    {
+        idx = mid;
+        firstOccurance(arr, target, s, mid - 1, idx);
+    }
+    else if (arr[mid] < target)
+    {
+        firstOccurance(arr, target, mid + 1, e, idx);
+    }
+    else
+    {
+        firstOccurance(arr, target, s, mid - 1, idx);
+    }
+}
+int main()
+{
+    int arr[] = {1, 3, 3, 3, 3, 4, 5, 6, 7};
+    //ITERATIVE  solution call
+    cout<<firstOccurance(arr,3,9);
+    //RECURSIVE SOLUTION CALL
+    // cout << firstOccurance(arr, 7, 0, 8, INT_MAX);
+
+    return 0;
 }
