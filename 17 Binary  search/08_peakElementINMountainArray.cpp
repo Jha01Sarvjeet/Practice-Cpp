5f9ec1402b47f5ef7b46b7bd9bb9d447e2301d91
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-08-09 22:56:35 +0530

    updated

--- 17 Binary  search/08_peakElementINMountainArray.cpp
+++ 17 Binary  search/08_peakElementINMountainArray.cpp
@@ -2,13 +2,13 @@
 #include<iostream>
 #include <bits/stdc++.h> 
 using namespace std;
-int peakElement(int arr[],int n){
+int peakElement(int nums[],int n){
     int s=0;
     int e=n-1;
     int mid=s+(e-s)/2;
     while(s<=e){
-        if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])return arr[mid];
-        else if(arr[mid]>arr[mid-1]){
+        if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])return nums[mid];
+        else if(nums[mid]>nums[mid-1]){
             s=mid+1;
         }
         else{
@@ -19,8 +19,8 @@ int peakElement(int arr[],int n){
 }
 int main(){
 int a,b,c;
-    int arr[]={1,5,4,0};
-    int peak_value=peakElement(arr,4);
+    int nums[]={1,5,4,0,6,9,3,2};
+    int peak_value=peakElement(nums,8);
     cout<<endl;
     cout<<peak_value;
      return 0;
