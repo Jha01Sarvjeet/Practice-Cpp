b809065e46017f75b387e0732dc7de4bca53d33d
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-09-10 11:16:38 +0530

    check that  two integers a and b such that a2 + b2 = c updated

--- 17 Binary  search/12_sumOfSquare.cpp
+++ 17 Binary  search/12_sumOfSquare.cpp
@@ -6,22 +6,36 @@
 using namespace std;
 
 // tc is not good
-bool isperfectsquare(int n){
-    if(sqrt(n)*sqrt(n)==n)return true;
-    return false;
-}
-bool judgeSquareSum(int c)
-{
-    int x = 0;
-    int y = c;
 
-    while (x<=y){
-        if(isperfectsquare(x) && isperfectsquare(y))return true;
-        x++;
-        y--;
+// bool isperfectsquare(int n){
+//     if(sqrt(n)*sqrt(n)==n)return true;
+//     return false;
+// }
+// bool judgeSquareSum(int c)
+// {
+//     int x = 0;
+//     int y = c;
+
+//     while (x<=y){
+//         if(isperfectsquare(x) && isperfectsquare(y))return true;
+//         x++;
+//         y--;
+//     }
+//     return false;
+
+// }
+
+//better approach
+bool judgeSquareSum(int c){
+    int x=0;
+    int y=sqrt(c);
+    while(x<=y){
+        if(x*x+y*y==c )return true;
+        else if(x*x+y*y<c)
+            x++;
+        else y--;
     }
     return false;
-
 }
 
 int main(){
