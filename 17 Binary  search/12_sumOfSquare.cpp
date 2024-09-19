632f0a6e5fdcf27a3d656f94a669fb3b2167c518
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-09-19 19:13:38 +0530

    sum of square updated

--- 17 Binary  search/12_sumOfSquare.cpp
+++ 17 Binary  search/12_sumOfSquare.cpp
@@ -1,4 +1,4 @@
-// to return that this number s square of sum of tum number
+// to return that this number s square of sum of two number
 
 // iterative approach
 #include<bits/stdc++.h>
@@ -6,11 +6,12 @@
 using namespace std;
 
 // tc is not good
+//1)first way 
 
-// bool isperfectsquare(int n){
-//     if(sqrt(n)*sqrt(n)==n)return true;
-//     return false;
-// }
+bool isperfectsquare(int n){
+    if(sqrt(n)*sqrt(n)==n)return true;
+    return false;
+}
 // bool judgeSquareSum(int c)
 // {
 //     int x = 0;
@@ -25,18 +26,39 @@ using namespace std;
 
 // }
 
-//better approach
+//2)second way
 bool judgeSquareSum(int c){
-    int x=0;
-    int y=sqrt(c);
-    while(x<=y){
-        if(x*x+y*y==c )return true;
-        else if(x*x+y*y<c)
-            x++;
-        else y--;
+    int a=0;
+    int b=c;
+    while(a<=b){
+        if(isperfectsquare(a) && isperfectsquare(b)){
+            return true;
+        }
+        else if (!isperfectsquare(b)){
+            b=(sqrt(b))*(sqrt(b));
+            a=c-b;
+           
+        }
+        else{
+             a=(sqrt(a)+1)*(sqrt(a)+1);
+            b=c-a;
+        }
     }
     return false;
 }
+
+//better approach
+// bool judgeSquareSum(int c){
+//     int x=0;
+//     int y=sqrt(c);
+//     while(x<=y){
+//         if(x*x+y*y==c )return true;
+//         else if(x*x+y*y<c)
+//             x++;
+//         else y--;
+//     }
+//     return false;
+// }
 
 int main(){
     cout<<judgeSquareSum(3);
