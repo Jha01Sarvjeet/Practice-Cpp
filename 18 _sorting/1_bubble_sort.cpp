0462f0e03943b5fcac6e1f22fffa3023b89e91bd
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-09-25 22:32:29 +0530

    bubble sort updated

--- 18 _sorting/1_bubble_sort.cpp
+++ 18 _sorting/1_bubble_sort.cpp
@@ -1,23 +1,40 @@
-//Bubble sort
-#include<bits/stdc++.h>
+// Bubble sort
+#include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-void bubbleSort(vector<int>&arr){
-    int n=arr.size();
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1])swap(arr[j],arr[j+1]);
+void bubbleSort(vector<int> &arr)
+{
+    bool flag = false;
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                flag = true;
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+        if(flag==false){
+            return;
+        }
+        else{
+            flag=false;
         }
     }
 }
-int main(){
-    vector<int>arr={5,4,3,2,1};
-    for(int i:arr){
-        cout<<i<<" ";
+int main()
+{
+    vector<int> arr = {8,7,6,5,4,3,2,1};
+    for (int i : arr)
+    {
+        cout << i << " ";
     }
-    cout<<endl;
+    cout << endl;
     bubbleSort(arr);
-     for(int i:arr){
-        cout<<i<<" ";
+    for (int i : arr)
+    {
+        cout << i << " ";
     }
 }
