1c27b05452abb5f9c6dbcda1971e3e34a8230bb7
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-09-25 22:10:50 +0530

    bubble sort

---
+++ 18 _sorting/1_bubble_sort.cpp
@@ -0,0 +1,23 @@
+//Bubble sort
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+void bubbleSort(vector<int>&arr){
+    int n=arr.size();
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1])swap(arr[j],arr[j+1]);
+        }
+    }
+}
+int main(){
+    vector<int>arr={5,4,3,2,1};
+    for(int i:arr){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    bubbleSort(arr);
+     for(int i:arr){
+        cout<<i<<" ";
+    }
+}
