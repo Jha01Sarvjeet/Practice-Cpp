6229a749557904f5451fa615c8b0c0e3d3d0154a
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-10-08 18:23:05 +0530

    updated with Boyer-Moore Majority Voting Algorithm

--- 18 _sorting/5_mazority_element.cpp
+++ 18 _sorting/5_mazority_element.cpp
@@ -6,15 +6,46 @@ The majority element is the element that appears more than ⌊n / 2⌋ times.
 Input: nums = [2,2,1,1,1,2,2]
 Output: 2
  */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-int main(){
-int num[]={2,2,1,1,1,2,2};
-int n=sizeof(num)/sizeof(num[0]);
-//sort the element and return the mid+1 th element
-sort(num,num+n);
+int main()
+{
+    int num[] = {2, 2, 1, 1, 1, 2, 2};
+    int n = sizeof(num) / sizeof(num[0]);
+    // sort the element and return the mid+1 th element
+    // better approach
+    //  sort(num,num+n);
 
-int mid=n/2;
-cout<<"mazority element in array is " <<num[mid+1]<<endl;
+    // int mid=n/2;
+    // cout<<"mazority element in array is " <<num[mid+1]<<endl;
+
+    // optimal approach(Boyer-Moore Majority Voting Algorithm)
+    int vote = 0;
+    int element;
+    for (int i = 0; i < n; i++)
+    {
+        if (vote == 0)
+        {
+            vote = 1;
+            element = num[i];
+        }
+        else if (num[i] == element)
+        {
+            vote++;
+        }
+        else{
+            vote--;
+        }
+    }
+    int count=0;
+    for(int i=0;i<n;i++){
+        if(element==num[i])count++;
+    }
+    if(count>n/2){
+        cout<<"mazority element "<<element<<endl;
+    }
+    else{
+        cout<<"no mazority element"<<endl;
+    }
 }
