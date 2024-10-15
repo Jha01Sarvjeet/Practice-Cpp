217ec0d66ef5e2a26d594826376255a246076967
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-10-15 22:27:26 +0530

    Minimum sum of two numbers formed from digits of an array

---
+++ 18 _sorting/11_minimumSumOfTwoNumber.cpp
@@ -0,0 +1,38 @@
+/*Given an array of digits (values are from 0 to 9), the task is to find the minimum
+ possible sum of two numbers formed from digits of the array. Please note that all digits
+  of the given array must be used to form the two numbers.*/
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int minimumSum(int arr[], int n)
+{
+    sort(arr, arr + n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int a = 0;
+    int b = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            a = a * 10 + arr[i];
+        }
+        else
+        {
+            b = b * 10 + arr[i];
+        }
+    }
+
+    return (a + b);
+}
+int main()
+{
+    int arr[] = {6, 8, 4, 5, 2, 3};
+    int minSum = minimumSum(arr, 6);
+    cout << "minimum sum of the two number formed  : " << minSum << endl;
+}
