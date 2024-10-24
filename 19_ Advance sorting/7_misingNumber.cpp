2ab1019485583b052822325b0dbfad083a7d1587
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-10-24 22:06:16 +0530

    Solved using Xor OPerator

--- 19_ Advance sorting/7_misingNumber.cpp
+++ 19_ Advance sorting/7_misingNumber.cpp
@@ -8,22 +8,37 @@ return the only number in the range that is missing from the array.*/
 #include <iostream>
 using namespace std;
 // Brute force approach
+// int missingValue(vector<int> arr)
+// {
+//     int n = arr.size();
+//     for (int i = 0; i <=n; i++)
+//     {
+//         bool flag = false;
+//         for (int j = 0; j < n; j++)
+//         {
+//             if (i == arr[j])
+//             {
+//                 flag = true;
+//             }
+//         }
+//         if (flag == false)
+//             return i;
+//     }
+// }
+
+// usng XOR operator
 int missingValue(vector<int> arr)
 {
     int n = arr.size();
+    int ans=0;
     for (int i = 0; i <=n; i++)
     {
-        bool flag = false;
-        for (int j = 0; j < n; j++)
-        {
-            if (i == arr[j])
-            {
-                flag = true;
-            }
-        }
-        if (flag == false)
-            return i;
+        ans=ans^i;
     }
+    for(int i=0;i<n;i++){
+        ans^=arr[i];
+    }
+    return ans;
 }
 int main()
 {
