ddbd5adbdf041a62018f984be5cf232e5ae53b39
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-10-24 19:34:21 +0530

    cycle Sort

---
+++ 19_ Advance sorting/6_cycleSort.cpp
@@ -0,0 +1,25 @@
+/*Cycle sort :- used in such case when range of value of array is known -- its complexity is O(N)*/
+/*eg:{5,1,3,2,4}*/
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+void cycleSort(vector<int>&arr){
+    int s=0;
+    int n=arr.size();
+    int i=0;
+    while(i<n){
+        int correctIndex=arr[i]-1;
+        if(i==correctIndex)i++;
+        else{
+            swap(arr[i],arr[correctIndex]);
+        }
+    }
+
+}
+int main(){
+    vector<int>arr={5,1,3,2,4};
+    for(auto i :arr)cout<<i<<" ";cout<<endl;
+    cycleSort(arr);
+    for(auto i :arr)cout<<i<<" ";cout<<endl;
+
+}
