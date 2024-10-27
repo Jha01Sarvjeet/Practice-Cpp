6b54716d1e075daac1e269f1142799b59060f7ce
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-10-27 10:48:25 +0530

    Reverse pair count

---
+++ 19_ Advance sorting/10_reversePair.cpp
@@ -0,0 +1,32 @@
+/*Given an integer array nums, return the number of reverse pairs in the array.
+
+A reverse pair is a pair (i, j) where:
+
+0 <= i < j < nums.length and
+nums[i] > 2 * nums[j].*/
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int reversePair(vector<int> arr)
+{
+    int count = 0;
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] > 2 * arr[j])
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+int main()
+{
+    vector<int>arr={2,4,3,5,1};
+    int revPairCount=reversePair(arr);
+    cout<<"Total reverse pair count :- "<<revPairCount<<endl;
+}
