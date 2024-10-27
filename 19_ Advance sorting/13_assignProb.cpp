8e33495e99c8b6ba83ad755f87dbe5c3fd5c2228
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-10-27 23:45:48 +0530

    duplicate value in array

---
+++ 19_ Advance sorting/13_assignProb.cpp
@@ -0,0 +1,34 @@
+/*Given an integer array nums of length n where all the integers of nums
+are in the range [1,n] and each integer appears once or twice, return an
+array of all the integers that appears twice. You must write an algorithm
+that runs in O(n) time and uses only constant extra space.*/
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+vector<int> duplicateNu(vector<int> arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        while (arr[i] != arr[arr[i] - 1])
+            swap(arr[i], arr[arr[i] - 1]);
+    }
+    for (auto i : arr)
+        cout << i << " ";
+    cout << endl;
+    vector<int> b;
+    for (int i = 0; i < n; i++)
+    {
+        if (i + 1 != arr[i])
+            b.push_back(arr[i]);
+    }
+    return b;
+}
+int main()
+{
+    vector<int> arr = {4, 3, 2, 7, 8, 2, 3, 1};
+    vector<int> ns = duplicateNu(arr);
+    for (auto i : ns)
+        cout << i << " ";
+}
