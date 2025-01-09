fa1e5113f10c5122c38fcad355b9aa6d42ea657a
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2025-01-09 18:14:04 +0530

    Decimal to binary (updated)

--- 22 Bitwise Operator and Modulo arithmetic/01_decimal_to_Binary.cpp
+++ 22 Bitwise Operator and Modulo arithmetic/01_decimal_to_Binary.cpp
@@ -2,15 +2,29 @@
 #include <iostream>
 using namespace std;
 /*to convert a binary string into Decimal Integer*/
+// int binaryToDecimal(string &str){
+//     int n=str.length();
+//     int result=0;
+//     int temp=1;
+//     for(int i=n-1;i>=0;i--){
+//         char ch=str[i];
+//         int num=ch-'0';
+//         result+=temp*num;
+//         temp*=2;
+
+//     }
+//     return result;
+// }
+
+//Another Approach without using temp
 int binaryToDecimal(string &str){
     int n=str.length();
     int result=0;
-    int temp=1;
     for(int i=n-1;i>=0;i--){
         char ch=str[i];
         int num=ch-'0';
-        result+=temp*num;
-        temp*=2;
+        result+=num*(1<<(n-i-1));
+       
 
     }
     return result;
