079b180dd3490ec463a6cf99bcacf7d4f2ad2136
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2025-01-09 19:27:32 +0530

    numberr of set bit(updated using builtin function0

--- 22 Bitwise Operator and Modulo arithmetic/03_numberOfSetBit.cpp
+++ 22 Bitwise Operator and Modulo arithmetic/03_numberOfSetBit.cpp
@@ -2,13 +2,18 @@
 #include <iostream>
 using namespace std;
 /*Number of set bit in a number*/
+// int noOfSet(int n){
+//     int count=0;
+//     while(n){
+//         if(n&1)count++;
+//         n=n>>1;
+//     }
+//     return count;
+// }
+
+//Using built in function
 int noOfSet(int n){
-    int count=0;
-    while(n){
-        if(n&1)count++;
-        n=n>>1;
-    }
-    return count;
+    return __builtin_popcount(n);
 }
 int main(){
     int n;
