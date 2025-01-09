eed86b9f434da535247d50ff7208436746934cb7
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2025-01-09 22:15:37 +0530

    maximum power of 2(updated with another Approach)

--- 22 Bitwise Operator and Modulo arithmetic/04_maximumPowerOf2.cpp
+++ 22 Bitwise Operator and Modulo arithmetic/04_maximumPowerOf2.cpp
@@ -3,22 +3,35 @@
 using namespace std;
 /*Maximum Power of 2 less than or equal to given number*/
 // we can say using somewhat (Brian Kernighan's Algorithm)
+// int maxPowerOf2(int n)
+// {
+//     int temp = 0;
+//     while (n>0)
+//     {
+//         if ((n & (n - 1)) != 0)
+//         {
+//             n = n & (n - 1);
+//         }
+//         else
+//         {
+//             temp = n;
+//             break;
+//         }
+//     }
+//     return temp;
+// }
+
+
+//Another approach
 int maxPowerOf2(int n)
 {
-    int temp = 0;
-    while (n>0)
-    {
-        if ((n & (n - 1)) != 0)
-        {
-            n = n & (n - 1);
-        }
-        else
-        {
-            temp = n;
-            break;
-        }
-    }
-    return temp;
+   //LET integer of 32 bit
+   n=n|(n>>1);
+   n=n|(n>>2);
+   n=n|(n>>4);
+   n=n|(n>>8);
+   n=n|(n>>16);
+   return ((n+1)>>1);
 }
 int main()
 {
