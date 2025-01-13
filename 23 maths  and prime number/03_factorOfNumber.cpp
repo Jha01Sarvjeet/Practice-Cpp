a5cd874aa5ac73c34dbc82981e5911fa33191859
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2025-01-13 18:57:48 +0530

    Fcators of given number(updated)

--- 23 maths  and prime number/03_factorOfNumber.cpp
+++ 23 maths  and prime number/03_factorOfNumber.cpp
@@ -2,9 +2,20 @@
 #include<bits/stdc++.h>
 #include <iostream>
 using namespace std;
+// void factor(int n){
+//     for(int i=1;i<=n;i++){
+//         if(n%i==0)cout<<i<<" ";
+//     }
+//     cout<<endl;
+// }
+
+//Updated
 void factor(int n){
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<sqrt(n);i++){
         if(n%i==0)cout<<i<<" ";
+    }
+       for(int i=sqrt(n);i>=1;i--){
+        if(n%i==0)cout<<n/i<<" ";
     }
     cout<<endl;
 }
