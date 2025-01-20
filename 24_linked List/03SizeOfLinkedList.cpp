cb6f3f3eed3f308ec08c2f80fb0f6c94d56c7a31
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2025-01-21 00:44:45 +0530

    finding size of Linked List and displaying linked list

--- 24_linked List/03SizeOfLinkedList.cpp
+++ 24_linked List/03SizeOfLinkedList.cpp
@@ -3,36 +3,48 @@
 using namespace std;
 class Node
 {
-    public:
+public:
     int val;
-    Node * next;
-    Node(int val){
-        this->val=val;
-        this->next  =NULL;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
     }
 };
+void display(Node *head)
+{
+
+    while (head != NULL)
+    {
+
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout<<endl;
+}
+int sizeOfLL(Node *head)
+{
+    int count = 0;
+    while (head != NULL)
+    {
+        count++;
+        head=head->next;
+    }
+    return count;
+}
 int main()
 {
-    Node *a=new Node(10);
-    Node *b=new Node(20);
-    Node *c=new Node(30);
-    Node *d=new Node(40);
-    a->next=b;          
-    b->next=c;          
-    c->next=d;
-    //printing linked list  and counting size of Linked list
-    Node *head=a;
-    int count=0;
-    while(head!=NULL){
-        count++;
-        cout<<head->val<<" ";
-        head=head->next;
-
-    }  
-    cout<<endl; 
-    cout<<"size of Linked list is "<<count<<endl;
-    
-   
-    
-
+    Node *a = new Node(10);
+    Node *b = new Node(20);
+    Node *c = new Node(30);
+    Node *d = new Node(40);
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    // printing linked list  and counting size of Linked list
+    Node *head = a;
+    display(head);
+    int siz = sizeOfLL(head);
+    cout << "size of Linked list is " << siz << endl;
 }
