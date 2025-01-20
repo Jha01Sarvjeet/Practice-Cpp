bb49e22f84d55d21027b28027c7f0dced6b09058
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2025-01-21 00:55:02 +0530

    reverse display of LL

---
+++ 24_linked List/04_reverseDisplay.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void rivDisplay(Node *head)
+{
+    if (head == NULL)
+        return;
+    rivDisplay(head->next);
+    cout << head->val << " ";
+}
+int sizeOfLL(Node *head)
+{
+    int count = 0;
+    while (head != NULL)
+    {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+int main()
+{
+    Node *a = new Node(10);
+    Node *b = new Node(20);
+    Node *c = new Node(30);
+    Node *d = new Node(40);
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    // printing linked list  and counting size of Linked list
+    Node *head = a;
+    rivDisplay(head);
+    int siz = sizeOfLL(head);
+    cout << endl
+         << "size of Linked list is " << siz << endl;
+}
