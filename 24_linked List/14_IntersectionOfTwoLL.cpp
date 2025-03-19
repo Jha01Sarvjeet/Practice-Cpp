ff29a753bdf1ea9f33b86a3a094140f96496ff08
Jha25Sarvjeet <jha.sarvjeet1@gmail.com>
2025-03-19 19:07:05 +0530

    updated(intersection of two ll)

--- 24_linked List/14_IntersectionOfTwoLL.cpp
+++ 24_linked List/14_IntersectionOfTwoLL.cpp
@@ -173,26 +173,18 @@ void intersectionOfLL(Node *head1, Node *head2)
             diff--;
         }
         Node *temp = head1;
-        while (temp != NULL)
+        while (head1 != head2)
         {
-            if (head1 == head2)
-            {
-                cout << head1->value << endl;
-                break;
-            }
-            else
-            {
-                temp = temp->next;
+        
                 head1 = head1->next;
                 head2 = head2->next;
-            }
+            
         }
-        if (temp == NULL)
-        {
-            cout << "No intersection";
-        }
+       
+        if(head1!=NULL){ cout <<head1->value;}
+        else cout<<"No Intersection";
+   
     }
-
 else
 {
     int diff = size2 - size1;
@@ -201,25 +193,18 @@ else
         head2 = head2->next;
         diff--;
     }
-    Node *temp = head1;
-    while (temp != NULL)
+    
+    while (head1 != head2)
     {
-        if (head1 == head2)
-        {
-            cout << head1->value << endl;
-            break;
-        }
-        else
-        {
-            temp = temp->next;
+    
             head1 = head1->next;
             head2 = head2->next;
-        }
+        
     }
-    if (temp == NULL)
-    {
-        cout << "No intersection";
-    }
+   
+       if(head1!=NULL){ cout <<head1->value;}
+       else cout<<"No Intersection";
+  
 }}
 
 int main()
