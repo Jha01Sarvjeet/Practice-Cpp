d97a1930fe96cae9d7c0a84f6d3d9b4438ce58e8
Jha25Sarvjeet <jha.sarvjeet1@gmail.com>
2025-06-18 23:56:57 +0530

    Iterative approach

--- 24_linked List/24_reverseList.cpp
+++ 24_linked List/24_reverseList.cpp
@@ -11,42 +11,62 @@ struct ListNode
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
-// first approach
+
 class Solution
 {
 public:
-    ListNode *nodeATIndex(ListNode *head, int n)
-    {
-        ListNode *temp = head;
-        for (int i = 0; i < n; i++)
-        {
-            temp = temp->next;
-        }
-        return temp;
+// first approach
+    // ListNode *nodeATIndex(ListNode *head, int n)
+    // {
+    //     ListNode *temp = head;
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         temp = temp->next;
+    //     }
+    //     return temp;
+    // }
+    // ListNode *reverseList(ListNode *head)
+    // {
+    //     ListNode *temp = head;
+    //     int n = 0;
+    //     while (temp != NULL)
+    //     {
+    //         n++;
+    //         temp = temp->next;
+    //     }
+    //     int i = 0;
+    //     int j = n - 1;
+    //     while (i <= j)
+    //     {
+    //         ListNode *left = nodeATIndex(head, i);
+    //         ListNode *right = nodeATIndex(head, j);
+    //         int temp = left->val;
+    //         left->val = right->val;
+    //         right->val = temp;
+    //         i++;
+    //         j--;
+    //     }
+    //     return head;
+    // }
+
+    //second approach
+      ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL)return head;
+       ListNode* prev=NULL;
+       ListNode* curr=head;
+       ListNode* nxt;
+       while(curr->next!=NULL){
+        nxt=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=nxt;
+
+
+       }
+       curr->next=prev;
+       return curr;
     }
-    ListNode *reverseList(ListNode *head)
-    {
-        ListNode *temp = head;
-        int n = 0;
-        while (temp != NULL)
-        {
-            n++;
-            temp = temp->next;
-        }
-        int i = 0;
-        int j = n - 1;
-        while (i <= j)
-        {
-            ListNode *left = nodeATIndex(head, i);
-            ListNode *right = nodeATIndex(head, j);
-            int temp = left->val;
-            left->val = right->val;
-            right->val = temp;
-            i++;
-            j--;
-        }
-        return head;
-    }
+
 };
 
 int main()
