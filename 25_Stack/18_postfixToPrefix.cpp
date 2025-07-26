a1ea2fdd0a624fd8285395d54615f6e277f6a317
Jha25Sarvjeet <jha.sarvjeet1@gmail.com>
2025-07-26 17:50:57 +0530

    postfix to prefix

---
+++ 25_Stack/18_postfixToPrefix.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+string calculate(string val1,string val2,char ch){
+    string s="";
+     s.push_back(ch);
+    s=s+ val1 ;
+   
+    s+=val2;
+    return s;
+}
+string postfixToPrefix(string str)
+{
+    stack<string> val;
+    for (int i = 0; i < str.length(); i++)
+    {
+        char ch = str[i];
+        if (ch >= 48 && ch <= 57)
+        {
+            val.push(to_string(ch-48));
+        }
+        else
+        {
+            string val2=val.top();
+            val.pop();
+            string val1=val.top();
+            val.pop();
+            string ans=calculate(val1,val2,ch);
+            val.push(ans);
+        }
+    }
+    return val.top();
+}
+int main()
+{
+    string s="79+4*8/3-";
+    cout<<postfixToPrefix(s)<<endl;
+}
